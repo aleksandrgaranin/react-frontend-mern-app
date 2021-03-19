ae23ced9e591d09e57547e9ca3300a2e5a0be6d2
aleksandGG <aleksandrgaranin89@gmail.com>
2021-03-19 14:12:12 -0700

    2 bugs fixed

--- src/places/components/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceItem/PlaceItem.js
@@ -92,7 +92,7 @@ const PlaceItem = props => {
           </div>
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-            {auth.isLoggedIn && (
+            {auth.userId === props.creatorId && (
               <React.Fragment>
                 <Button to={`/places/${props.id}`}>EDIT</Button>
                 <Button danger onClick={openDeleteWarningHandler}>DELETE</Button>
--- src/user/pages/Auth/Auth.js
+++ src/user/pages/Auth/Auth.js
@@ -127,8 +127,8 @@ const Auth = () => {
             id="password"
             element="input"
             label="Password"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Pleace enter a valid password (at least 5 characters)."
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Pleace enter a valid password (at least 6 characters)."
             onInput={inputHandler}
           />
           <Button type="submit" disabled={!formState.isValid}>
